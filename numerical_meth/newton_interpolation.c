4d92611604e6c000aefa01070a495a15056a65a6
pes18fan <pes18fan@disr.it>
2024-12-22 16:28:32 +0545

    feat: add newton interpolation

---
+++ numerical_meth/newton_interpolation.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("How many points? ");
+    scanf("%d", &n);
+
+    double xp;
+
+    printf("Provide the input (xp): ");
+    scanf("%lf", &xp);
+
+    double x[n], a[n][n];
+    for (int i = 0; i < n; i++) {
+        printf("Provide x[%d] and a[0][%d]: ", i, i);
+        scanf("%lf %lf", &x[i], &a[0][i]);
+    }
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < n - i; j++) {
+            a[i][j] = (a[i - 1][j + 1] - a[i - 1][j]) / (x[i + j] - x[j]);
+        }
+    }
+
+    double p = a[0][0];
+    double product;
+
+    for (int i = 1; i < n; i++) {
+        product = 1;
+        for (int j = 0; j < i; j++) {
+            product *= xp - x[j];
+        }
+        p += a[i][0] * product;
+    }
+
+    printf("Result is %lf", p);
+    return 0;
+}
