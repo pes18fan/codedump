118ba52644744cedca86915b1babbf086d594e5d
pes18fan <pes18fan@disr.it>
2025-01-08 20:17:13 +0545

    feat: add infix-postfix converter

---
+++ dsa/4th_sem_dsa/infix_to_postfix.c
@@ -0,0 +1,173 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef enum { LR, RL } Associativity;
+
+typedef struct {
+    char* arr;
+    int capacity;
+    int top;
+} Stack;
+
+void init_stack(Stack* s) {
+    s->capacity = 100;
+    s->arr = malloc(s->capacity);
+    s->top = -1;
+}
+
+void destroy_stack(Stack* s) {
+    free(s->arr);
+    s->arr = NULL;
+}
+
+void push(Stack* s, char value) {
+    if (s->top >= s->capacity - 1) {
+        s->capacity *= 2;
+        char* temp = realloc(s->arr, s->capacity);
+        if (!temp) {
+            fprintf(stderr, "Failed to allocate memory\n");
+            free(s->arr);
+            exit(EXIT_FAILURE);
+        }
+        s->arr = temp;
+    }
+
+    s->top++;
+    s->arr[s->top] = value;
+}
+
+bool is_empty(Stack* s) { return s->top < 0; }
+
+char pop(Stack* s) {
+    if (is_empty(s)) {
+        printf("Stack is empty!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    char popped = s->arr[s->top];
+    s->top--;
+    return popped;
+}
+
+char peek(Stack* s) {
+    if (is_empty(s)) {
+        printf("Stack is empty!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return s->arr[s->top];
+}
+
+int precedence_of(char c) {
+    switch (c) {
+    case '(':
+    case ')':
+        return 3;
+    case '^':
+        return 2;
+    case '/':
+    case '*':
+        return 1;
+    case '+':
+    case '-':
+        return 0;
+    default:
+        return -1;
+    }
+}
+
+Associativity associativity_of(char c) {
+    switch (c) {
+    case '^':
+        return RL;
+    case '/':
+    case '*':
+    case '+':
+    case '-':
+        return LR;
+    default:
+        return -1;
+    }
+}
+
+bool is_digit(char c) { return c >= 48 && c <= 57; }
+
+void print_postfix(char* infix) {
+    Stack stack;
+    init_stack(&stack);
+
+    for (int i = 0; infix[i] != '\0'; i++) {
+        char c = infix[i];
+
+        if (is_digit(c)) {
+            printf("%c ", c);
+            continue;
+        }
+
+        switch (c) {
+        case '^':
+        case '/':
+        case '*':
+        case '+':
+        case '-': {
+            if (is_empty(&stack) || peek(&stack) == '(') {
+                push(&stack, c);
+                continue;
+            }
+
+            // Higher number, higher precedence
+            // 3: () {} [], 2: ^, 1: / *, 0: + -
+            if (precedence_of(c) > precedence_of(peek(&stack))) {
+                push(&stack, c);
+            } else if (precedence_of(c) < precedence_of(peek(&stack))) {
+                printf("%c ", pop(&stack));
+                push(&stack, c);
+            } else {
+                if (associativity_of(c) == LR) {
+                    printf("%c ", pop(&stack));
+                    push(&stack, c);
+                } else {
+                    push(&stack, c);
+                }
+            }
+
+            break;
+        }
+        case '(':
+            push(&stack, c);
+            break;
+        case ')': {
+            if (is_empty(&stack)) {
+                fprintf(stderr, "Invalid infix input\n");
+                goto end;
+            }
+
+            char popped;
+            while (true) {
+                popped = pop(&stack);
+                if (popped == '(') {
+                    break;
+                }
+
+                printf("%c ", popped);
+            }
+        }
+        case ' ':
+            continue;
+        default:
+            fprintf(stderr, "\nInvalid character %c\n", c);
+            goto end;
+        }
+    }
+
+    while (!is_empty(&stack)) {
+        printf("%c ", pop(&stack));
+    }
+
+end:
+    destroy_stack(&stack);
+    return;
+}
+
+int main() { print_postfix("1 + (2 - 3) * 7 / 8"); }
