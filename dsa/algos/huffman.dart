5593e15f456b19784644ae1256e003f224d87e85
pes18fan <pes18fan@disr.it>
2025-01-22 14:18:05 +0545

    feat: add decoding for huffman

--- dsa/algos/huffman.dart
+++ dsa/algos/huffman.dart
@@ -11,35 +11,70 @@ class HuffmanNode {
   HuffmanNode(this.character, this.frequency);
 }
 
-String? _findCharacter(HuffmanNode? node, String needle, String path) {
-  // character not found
+/* A map of each character to its code */
+var characterMap = <String, String>{};
+
+String? _findCharacterCode(HuffmanNode? node, String needle, String path) {
+  // character doesn't exist in the tree
   if (node == null) {
     return null;
   }
 
-  // found the character!
+  // found the character! Now, return the associated code
   if (node.character != null && node.character == needle) {
+    characterMap[needle] = path;
     return path;
   }
 
-  var left = _findCharacter(node.left, needle, path + "0");
+  var left = _findCharacterCode(node.left, needle, path + "0");
   if (left != null) {
-    return left; // either the left path or null
+    return left; // the left path
   }
 
-  var right = _findCharacter(node.right, needle, path + "1");
+  var right = _findCharacterCode(node.right, needle, path + "1");
   return right; // either the right path or null
 }
 
-String? findCharacter(HuffmanNode? root, String needle) {
+String? findCharacterCode(HuffmanNode root, String needle) {
   var path = "";
 
-  return _findCharacter(root, needle, path);
+  return _findCharacterCode(root, needle, path);
 }
 
-String huffman(String text) {
+(String?, String) _findCharacter(HuffmanNode? node, String code) {
+  if (node == null) {
+    return (null, code);
+  }
+
+  /* Found the character. Why? Because we reached a leaf node. */
+  if (node.left == null && node.right == null) {
+    return (node.character, code);
+  }
+
+  /* See where the code's telling us to go */
+  if (code[0] == "0") {
+    return _findCharacter(node.left, code.substring(1));
+  } else if (code[0] == "1") {
+    return _findCharacter(node.right, code.substring(1));
+  } else {
+    /* Invalid Huffman code as it has something other than a zero or one */
+    return (null, code);
+  }
+}
+
+/* Return the character we're looking for (first string) and the remaining code
+ * that we sent (second string). */
+(String?, String) findCharacter(HuffmanNode root, String code) {
+  if (code.isEmpty) {
+    throw ArgumentError("Code can't be empty!");
+  }
+
+  return _findCharacter(root, code);
+}
+
+HuffmanNode getHuffmanTree(String text) {
   // calculate character frequency
-  var charFrequency = Map<String, int>();
+  var charFrequency = <String, int>{};
 
   for (int i = 0; i < text.length; i++) {
     var character = text[i];
@@ -74,20 +109,63 @@ String huffman(String text) {
     queue.sort((a, b) => a.frequency.compareTo(b.frequency));
   }
 
-  // now traverse it to form the code
+  return root;
+}
+
+String encode(String text, [HuffmanNode? treeRoot]) {
+  treeRoot ??= getHuffmanTree(text);
+
+  // traverse the tree to form the code
   var sb = StringBuffer();
   for (int i = 0; i < text.length; i++) {
     var character = text[i];
-    String charCode = findCharacter(root, character)!;
+
+    if (characterMap[i] != null) {
+      sb.write(characterMap[i]);
+      continue;
+    }
+
+    var charCode = findCharacterCode(treeRoot, character);
+    if (charCode == null) {
+      throw ArgumentError("Invalid Huffman tree provided.");
+    }
+
     sb.write(charCode);
   }
 
   return sb.toString();
 }
 
+String decode(String code, HuffmanNode treeRoot) {
+  var sb = StringBuffer();
+
+  while (true) {
+    var (character, updatedCode) = findCharacter(treeRoot, code);
+    code = updatedCode;
+
+    if (character == null) {
+      throw ArgumentError("Invalid Huffman code or Huffman tree provided.");
+    }
+
+    sb.write(character);
+
+    if (code.isEmpty) {
+      break;
+    }
+  }
+
+  return sb.toString();
+}
+
 void main() {
-  var str = "huffman coding exampleeeee";
-  String code = huffman(str);
+  var str = "aaabbbbcccccdd";
+
+  var root = getHuffmanTree(str);
+  String code = encode(str, root);
+
   print("Number of bits in original string: ${str.length * 8}");
   print("Number of bits in compressed string: ${code.length}");
+
+  String original = decode(code, root);
+  print("Decoded as: $original");
 }
